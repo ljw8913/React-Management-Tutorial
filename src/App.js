c009461509a9974a69cde1f6dbe1a323e528cf1a
Jiwon Lee <jiwoon@gamevilcom2us.com>
2020-05-28 11:38:40 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Customer from './components/Customer';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React Project
-        </p>
-      </header>
-    </div>
-  );
+
+
+const customers = [
+  {
+  'id':1,
+  'image':'https://placeimg.com/64/64/1',
+  'name':'이지원',
+  'birthday':'940202',
+  'gender':'여자',
+  'job':'대학생'
+},
+{
+'id':2,
+'image':'https://placeimg.com/64/64/2',
+'name':'이지원',
+'birthday':'940202',
+'gender':'여자',
+'job':'대학생'
+},
+{
+'id':3,
+'image':'https://placeimg.com/64/64/3',
+'name':'이지원',
+'birthday':'940202',
+'gender':'여자',
+'job':'대학생'
+},
+]
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        {
+          customers.map(c => {
+            return (
+            <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+            />
+            );
+          })
+        }
+      </div>
+    )
+  }
 }
 
 export default App;
